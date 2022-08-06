68d4e84d05fe13c6a2031c4024a43d8c1b60c818
Gabriel Cardoso <gr78787@gmail.com>
2022-08-06 20:59:08 -0300

    Edita Heroi two way data biding

---
+++ anotation/readme..md
@@ -0,0 +1,5 @@
+# Angular Tour-of-Heroes
+
+## ng builder
+Compila todo nosso código para produção, converte arquivos TS em JS
+SCSS em CSS, etc..
---
+++ src/app/hero.model.ts
@@ -0,0 +1,6 @@
+// A interface está neste local para se exportadas poesteriormente por outros arquivos
+
+export interface IHero{
+  id: number;
+  name: string;
+}
